e41a44fbcedc33732b8b055acc7f6c080119c183
Ethan <Ethan@Ethan-Laptop.unl.edu>
2024-08-29 13:36:40 -0500

    Added Major to file

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@
 public class Hello {
 	 public static void main(String args[]) {
 		    System.out.println("My name is Ethan Steele");
+		    System.out.println("My major is Computer Science!");
 		  }
 }
