d65002c7ca79ebab2d9c14db224d6a5b9792744a
Ethan <Ethan@Ethan-Laptop.unl.edu>
2024-09-03 16:13:34 -0500

    Added "partner" lines

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,10 @@ public class Hello {
 	 public static void main(String args[]) {
 		    System.out.println("My name is Ethan Steele");
 		    System.out.println("My major is Computer Science!");
+		    
+		    //this code was done by myself!
+		    
+		    System.out.println("My name is Alan Turing");
+		    System.out.println("I invented this guy's major!");
 		  }
 }
